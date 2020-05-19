133520194e5c17c9f826a7e8a386e391ec360923
Jon Magnuson <jon.magnuson@gmail.com>
2020-05-18 22:43:08 -0500

    debugging stuff
    enqueue_node is returning false (probably because Poll is not actually
    asleep), so wake doesn't actually propagate :(

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -3,7 +3,8 @@ use futures::future::{TryFutureExt, FutureExt};
 //use futures_util::future::try_future::TryFutureExt;
 use tokio_libevent::{TokioLibevent};
 use tokio::park::Unpark;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
 
 struct BaseWrapper(pub TokioLibevent);
 
@@ -27,34 +28,41 @@ fn main() {
 
     //let mut rt = Runtime::new().expect("failed to make the runtime");
     println!("hi");
-    let mut libevent = BaseWrapper(
+    let mut libevent = Arc::new(Mutex::new(BaseWrapper(
         TokioLibevent::new()
         .unwrap_or_else(|e| panic!("{:?}", e))
-    );
+    )));
 
-    let _ = unsafe { libevent.0.inner_mut().with_base(|base| {
+    let _ = unsafe { libevent.lock().unwrap().inner_mut().inner_mut().with_base(|base| {
         ffi::helloc_init(base)
     })};
 
     //let ughh = libevent.as_fd().fd;
-    let ughh = unsafe { libevent.0.inner_mut().base_mut().as_inner_mut() };
+    let ughh = unsafe { libevent.lock().unwrap().inner_mut().inner_mut().base_mut().as_inner_mut() };
 
+    let libevent_clone = libevent.clone();
     let mut rt = Builder::new()
         .basic_scheduler()
         .enable_all()
         .with_park(move |maybe_duration| {
-            let libevent_ref = libevent.0.inner_mut();
+            let mut libevent_guard = libevent_clone.lock().unwrap();
+            let libevent_ref = libevent_guard.inner_mut().inner_mut();
+            println!("******* PARKING *******");
+            let now = Instant::now();
             let new_duration = if let Some(duration) = maybe_duration {
-                libevent_ref.run_until_event(Some(duration));
+                // libevent_ref.run_until_event(Some(duration));
+                libevent_ref.run_timeout(Duration::from_secs(10));
 
-                // Some(Duration::from_secs(0))
-                None
+                Some(Duration::from_millis(1))
+                // None
             } else {
-                libevent_ref.run_until_event(Some(Duration::from_secs(1)));
+                // libevent_ref.run_until_event(Some(Duration::from_secs(1)));
+                libevent_ref.run_timeout(Duration::from_secs(10));
 
-                // Some(Duration::from_secs(0))
-                None
+                Some(Duration::from_millis(1))
+                // None
             };
+            println!("           exiting park: {}s          ", now.elapsed().as_secs());
             new_duration
             // Some(new_duration)
         })
@@ -66,12 +74,14 @@ fn main() {
 
     let mut a: usize = 0;
 
-    let _ev = libevent.inner_mut().inner_mut().add_interval(
-        Duration::from_secs(6),
+    let _ev = libevent.lock().unwrap().inner_mut().inner_mut().add_interval(
+        Duration::from_secs(2),
         move |_ev, _flags| {
             a += 1;
+            println!("_________ UNPARKING _________");
+            //handle.driver_handle().wakeup();
             handle.driver_handle().unpark();
-            println!("interval count: {}, flags: {:?}", a, _flags);
+            // println!("interval count: {}, flags: {:?}", a, _flags);
         }
     );
 
